489019ab6ce387a7ebcbcd57ad8514058c233385
Ainun27 <aidahpermana27@gmail.com>
2024-11-06 15:15:46 +0700

    Add files via upload

--- app/src/main/java/com/example/myapplication/view/main/MainActivity.java
+++ app/src/main/java/com/example/myapplication/view/main/MainActivity.java
@@ -4,37 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import com.example.myapplication.R;
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import com.example.myapplication.view.history.HistoryActivity;
-import com.example.myapplication.view.input.DataKapalActivity;
-import com.example.myapplication.view.input.DataKeretaActivity;
-import com.example.myapplication.view.input.DataPesawatActivity;
 import android.widget.ImageView;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.imageProfile).setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+        findViewById(R.id.btn_start).setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SplashScreen.class);
             startActivity(intent);
         });
 
-        findViewById(R.id.cvKapal).setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, DataKapalActivity.class);
-            startActivity(intent);
-        });
-        findViewById(R.id.cvPesawat).setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, DataPesawatActivity.class);
-            startActivity(intent);
-        });
-        findViewById(R.id.cvKereta).setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, DataKeretaActivity.class);
-            startActivity(intent);
-        });
     }
 }
